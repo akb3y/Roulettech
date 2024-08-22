0560a74500643e4640e7339d96be330a606b441b
alisha barraw <alisha.barraw@gmail.com>
2024-08-21 19:49:57 -0600

    created db schema

--- server/inventory/models.py
+++ server/inventory/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Inventory(models.Model):
+    name = models.CharField(max_length=20)
+    quantity = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}: {self.description}"
+    
+    def get_quantity(self):
+        return self.quantity
