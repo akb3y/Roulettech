d83531abd1d9cf7452ea401736bd9c1c220feb38
alisha barraw <alisha.barraw@gmail.com>
2024-08-23 10:58:58 -0600

    updated model to remove description

--- server/inventory/models.py
+++ server/inventory/models.py
@@ -3,10 +3,9 @@ from django.db import models
 class Inventory(models.Model):
     name = models.CharField(max_length=20)
     quantity = models.IntegerField()
-    description = models.TextField()
 
     def __str__(self):
-        return f"{self.name}: {self.description}"
+        return f"{self.name}"
     
     def get_quantity(self):
         return self.quantity
