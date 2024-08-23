3e1635e6af360255cf132884f74373dc8a04f650
alisha barraw <alisha.barraw@gmail.com>
2024-08-23 10:59:40 -0600

    removed description and unnecessary packages

--- server/inventory/views.py
+++ server/inventory/views.py
@@ -1,8 +1,7 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import F
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
+from django.shortcuts import get_object_or_404
 
 from .models import Inventory
 
@@ -12,7 +11,7 @@ def index(request):
 
 @csrf_exempt
 def get_all_items(request):
-    inventory_list = Inventory.objects.all().values('id', 'name', 'quantity', 'description')
+    inventory_list = Inventory.objects.all().values('id', 'name', 'quantity')
     return JsonResponse(list(inventory_list), safe=False)
 
 @csrf_exempt
@@ -21,20 +20,18 @@ def create_item(request):
         try:
             name = request.POST.get('name')
             quantity = request.POST.get('quantity')
-            description = request.POST.get('description', '')
 
             if not name:
                 return HttpResponseBadRequest("Name is required")
             if int(quantity) < 0:
                 return HttpResponseBadRequest("Quantity cannot be negative")
             
-            inventory = Inventory.objects.create(name=name, quantity=quantity, description=description)
+            inventory = Inventory.objects.create(name=name, quantity=quantity)
 
             response_data = {
                 'id': inventory.id,
                 'name': inventory.name,
                 'quantity': inventory.quantity,
-                'description': inventory.description,
             }
             return JsonResponse(response_data, status=201)
 
